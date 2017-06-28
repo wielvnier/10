7ec4b2b26e32e1922403d76a55b4bb22262f0e88
Michał Gacek <gacek.michael@outlook.com>
2017-06-28 21:44:18 +0200

    Improvements

--- scripts.js
+++ scripts.js
@@ -1,9 +1,27 @@
 $(function() {
-	var carouselList = $("#carousel ul");
-    setInterval(changeSlide, 3000);
+	var $carousel = $('#carousel'),
+        $prev = $('#prev'),
+        $next = $('#next'),
+        carouselList = $("#carousel ul"),
+        interval;
 
-    function changeSlide() {
+    interval = setInterval(moveNext, 3000);
+
+    function moveNext() {
         carouselList.animate({'marginLeft':-500}, 500, moveFirstSlide);
+    }
+
+    function movePrev() {
+        moveLastSlide();
+        carouselList.animate({'marginLeft':0}, 500);
+    }
+
+    function moveLastSlide() {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+
+        firstItem.before(lastItem);
+        carouselList.css({marginLeft: -500});
     }
 
     function moveFirstSlide() {
@@ -12,4 +30,21 @@ $(function() {
         lastItem.after(firstItem);
         carouselList.css({marginLeft:0});
     }
+
+    $carousel.hover(
+        function() {
+            clearInterval(interval);
+        },
+        function() {
+            interval = setInterval(moveNext, 3000);
+        }
+    );
+
+    $next.click(function() {
+        moveNext();
+    });
+
+    $prev.click(function() {
+        movePrev();
+    });
 });
